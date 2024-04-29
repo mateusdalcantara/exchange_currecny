e5afa5d82d0fc4a2c50bf6656ec7acf435af1875
Mateus Alcantara <121315239+mateusdalcantara@users.noreply.github.com>
2024-04-29 17:38:16 -0300

    Update Principal.java

--- src/br/com/example/principal/Principal.java
+++ src/br/com/example/principal/Principal.java
@@ -3,13 +3,14 @@ package br.com.example.principal;
 import br.com.example.entites.Data;
 import br.com.example.entites.ApiHandler;
 import br.com.example.entites.Messager;
-
+import java.util.Locale;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) {
-
+        
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         Data data = new Data();
         Messager messager = new Messager();
